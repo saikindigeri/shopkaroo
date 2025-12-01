5f45569107c5390ff87ce0479936a383c7a3cba1
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-12-01 12:44:57 +0530

    added deployed frontend url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://shopkaroo-pdso.onrender.com', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
  
